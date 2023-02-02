5d17eb83843d577b76183007cc87a7ff71c59285
minsukwon <libertyfromthinking@gmail.com>
2023-02-02 23:58:07 +0900

    회원가입 또는 로그인 기능

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,25 +1,32 @@
 import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { authService as auth} from 'fbase';
 
 const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [newAccount, setNewAccount] = useState(false);
 
   const onChange = (event) => {
-    const {target:{value, name}} = event;
-    // const {value, name} = target;
+    const { target : { value, name } } = event;
 
-    if(name=='email'){
+    if(name==='email'){
       setEmail(value);
       console.log(value);
     }else{
       setPassword(value);
       console.log(value);
     }
-    
   }
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      const data = newAccount ? await createUserWithEmailAndPassword(auth, email, password) : await signInWithEmailAndPassword(auth, email, password);
+      console.log(`데이타 :${data}`);
+    } catch(error) {
+      console.log(`에라 : ${error}`);
+    }
   }
 
   return (
@@ -27,7 +34,7 @@ const Auth = () => {
       <form onSubmit={onSubmit}>
         <input type='text' placeholder='Email' name='email' value={email} onChange={onChange} required/>
         <input type='password' placeholder='Password' name='password' value={password} onChange={onChange} required/>
-        <input type='submit' value='Login' />
+        <input type='submit' value={newAccount ? 'Sign Up' : 'Sign In'} />
       </form>
       <div>
         <button>Continue with Google</button>
