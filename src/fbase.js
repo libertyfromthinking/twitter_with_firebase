c708100f436e8a7309ba74cc08726371bdd1b559
minsukwon <libertyfromthinking@gmail.com>
2023-02-12 13:09:22 +0900

    파이어베이스 연결 이슈로 환경변수 변경
    - .env 파일 사용 시 undefined로 불러오는 이슈 있음
    - 임시로 .env파일 사용하지 않음

--- src/fbase.js
+++ src/fbase.js
@@ -2,14 +2,24 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_DATABASE_URL,
+//     storageBucket: process.env.REACT_APP_PROJECT_ID,
+//     messagingSenderId: process.env.REACT_APP_STORAGE_BUCKET,
+//     appId: process.env.REACT_APP_APP_ID,
+// };
+
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_DATABASE_URL,
-    storageBucket: process.env.REACT_APP_PROJECT_ID,
-    messagingSenderId: process.env.REACT_APP_STORAGE_BUCKET,
-    appId: process.env.REACT_APP_APP_ID,
-  };
+  apiKey: "AIzaSyBWGmuBvjKcTxuTHIH8tbGY9P8qD5bY_u4",
+  authDomain: "nwitter-279c4.firebaseapp.com",
+  databaseURL: "https://nwitter-279c4-default-rtdb.firebaseio.com",
+  projectId: "nwitter-279c4",
+  storageBucket: "nwitter-279c4.appspot.com",
+  messagingSenderId: "136716293192",
+  appId: "1:136716293192:web:64dbd3f3a21ce5f33f747d"
+};
 
 const app = initializeApp(firebaseConfig);
 export const authService = getAuth();
