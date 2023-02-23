28134abeeb38155360b4a48b74de7111271e095a
minsukwon <libertyfromthinking@gmail.com>
2023-02-24 01:55:10 +0900

    Profile 화면 user의 nweet 가져오기

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,6 +1,24 @@
-import React from "react";
+import { dbService, nweetCollectionRef } from "fbase";
+import { getDocs, orderBy, query, where } from "firebase/firestore";
+import React, { useEffect } from "react";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
+  const getNweets = async () => {
+    const q = await query(
+      nweetCollectionRef,
+      where("creatorId", "==", userObj.uid),
+      orderBy("createdAt"),
+    );
+    const nweets = await getDocs(q);
+
+    nweets.forEach((doc) => {
+      console.log(doc.data());
+    });
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
   return <span>Profile</span>;
 };
 
